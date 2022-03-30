78aa6745b3e2fbf3d978535931aeeecfda694430
Subhan Mahmood <subhan70m@gmail.com>
2022-03-30 10:47:28 +0100

    feat: got output of changelog

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const args = process.argv.slice(2);
-console.log(args[0])
-console.log(args[1])
-console.log(args[1])
+const { generateStringChangelog } = require("./test");
+
+function getSecondIdxOfVersionTags(arr) {
+    const titles = arr.filter(item => /^.*[0-9]+\.[0-9]+\.[0-9]+\s\([0-9]{4}-[0-9]{2}-[0-9]{2}\).*/.test(item) && item)
+    return arr.indexOf(titles[1])
+}
+
+async function main() {
+  let changelog = await generateStringChangelog("v");
+  changelog = changelog.split("\n").filter((line) => line !== "")
+  const idx = getSecondIdxOfVersionTags(changelog)
+
+  console.log(changelog.slice(0, idx).join('\n'))
+}
+main()
--- test.js
+++ test.js
@@ -1,12 +1,67 @@
-var conventionalChangelogCore = require('conventional-changelog-core');
-const { Writable } = require('stream');
+const fs = require('fs')
+const conventionalChangelog = require('conventional-changelog')
 
-const outStream = new Writable({
-    write(chunk, encoding, callback) {
-      console.log("input", chunk.toString());
-      callback();
-    }
-  });
+/**
+ * Generates a changelog stream with the given arguments
+ *
+ * @param tagPrefix
+ * @param preset
+ * @param version
+ * @param releaseCount
+ * @returns {*}
+ */
+const getChangelogStream = (tagPrefix, preset, version, releaseCount, config) => conventionalChangelog({
+    preset,
+    releaseCount: parseInt(releaseCount, 10),
+    tagPrefix,
+    config
+  },
+  {
+    version,
+    currentTag: `${tagPrefix}${version}`,
+  },
+  {},
+  config && config.parserOpts,
+  config && config.writerOpts
+)
 
-conventionalChangelogCore()
-  .pipe(outStream); // or any writable stream
+module.exports = getChangelogStream
+
+/**
+ * Generates a string changelog
+ *
+ * @param tagPrefix
+ * @param preset
+ * @param version
+ * @param releaseCount
+ * @returns {Promise<string>}
+ */
+module.exports.generateStringChangelog = (tagPrefix, preset, version, releaseCount, config) => new Promise((resolve, reject) => {
+  const changelogStream = getChangelogStream(tagPrefix, preset, version, releaseCount, config)
+
+  let changelog = ''
+
+  changelogStream
+    .on('data', (data) => {
+      changelog += data.toString()
+    })
+    .on('end', () => resolve(changelog))
+})
+
+/**
+ * Generates a file changelog
+ *
+ * @param tagPrefix
+ * @param preset
+ * @param version
+ * @param fileName
+ * @param releaseCount
+ * @returns {Promise<>}
+ */
+module.exports.generateFileChangelog = (tagPrefix, preset, version, fileName, releaseCount, config) => new Promise((resolve) => {
+  const changelogStream = getChangelogStream(tagPrefix, preset, version, releaseCount, config)
+
+  changelogStream
+    .pipe(fs.createWriteStream(fileName))
+    .on('finish', resolve)
+})
